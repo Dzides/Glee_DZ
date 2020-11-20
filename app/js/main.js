128a12446ed29365e7763e12b8491145e45abcba
Dzides <idzigun@.ru>
2020-11-21 02:37:12 +0300

    slider section added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-    
+    $('.slider__items').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpead: 3000
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,7 @@ function styles() {
 function scripts() {
     return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
