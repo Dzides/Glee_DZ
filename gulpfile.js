f52b3775ac892bef84e663a19014b92428055a07
Dzides <idzigun@.ru>
2020-11-21 22:39:58 +0300

    vedeo popup added

--- gulpfile.js
+++ gulpfile.js
@@ -40,8 +40,9 @@ function styles() {
 function scripts() {
     return src([
     'node_modules/jquery/dist/jquery.js',
+    'app/js/main.js',
     'node_modules/slick-carousel/slick/slick.js',
-    'app/js/main.js'
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
     ])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
