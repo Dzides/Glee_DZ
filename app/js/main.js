31b20885b3749a4c515c92cdc4775434ac975e9c
Dzides <idzigun@.ru>
2020-11-24 21:38:35 +0300

    design section added

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,13 @@
-$(function(){
-    $('.slider__items').slick({
-        dots: true,
-        arrows: false,
-        fade: true,
-        autoplay: true,
-        autoplaySpead: 3000
-    });
+$(function () {
+        $('.slider__items').slick({
+            dots: true,
+            arrows: false,
+            fade: true,
+            autoplay: true,
+            autoplaySpead: 3000
+        });
 
-    var mixer = mixitup('.products__cards');
+        var mixer1 = mixitup('.products__cards', {selectors: {control: '.products__btn'}});
+        var mixer2 = mixitup('.design__cards', {selectors: {control: '.design__btn'}});
+
 });
