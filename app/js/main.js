88cd719c46c3d976e543a795df9e6981b11329d2
Dzides <idzigun@.ru>
2020-12-03 21:53:39 +0300

    section about added

--- app/js/main.js
+++ app/js/main.js
@@ -2,9 +2,9 @@ $(function () {
         $('.slider-main__items').slick({
             dots: true,
             arrows: false,
-            // fade: true,
-            // autoplay: true,
-            // autoplaySpead: 3000
+            fade: true,
+            autoplay: true,
+            autoplaySpead: 3000
         });
 
         var mixer1 = mixitup('.products__cards', {selectors: {control: '.products__btn'}});
