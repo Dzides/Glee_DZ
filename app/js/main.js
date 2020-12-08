f45d9292a0162b477aebd4c7f5440c2fa2cce037
Dzides <idzigun@.ru>
2020-12-08 23:04:28 +0300

    adapt header

--- app/js/main.js
+++ app/js/main.js
@@ -2,10 +2,23 @@ $(function () {
         $('.slider-main__items').slick({
             dots: true,
             arrows: false,
-            fade: true,
-            autoplay: true,
-            autoplaySpead: 3000
+            // fade: true,
+            // autoplay: true,
+            // autoplaySpead: 3000
         });
+
+        $('.menu a, .panel-mobil__link').on('click', function (e) {
+            e.preventDefault();
+            var id = $(this).attr('href'),
+              top = $(id).offset().top;
+            $('body,html').animate({
+              scrollTop: top
+            }, 1500);
+          });
+
+          $('.header__btn').on('click', function () {
+            $('.menu__list').toggleClass('menu__list--active');
+          });
 
         var mixer1 = mixitup('.products__cards', {selectors: {control: '.products__btn'}});
         var mixer2 = mixitup('.design__cards', {selectors: {control: '.design__btn'}});
--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,7 @@ function browsersync() {
     })
 }
 
+
 function styles() {
     return src('app/scss/style.scss')
     .pipe(scss({outputStyle: 'compressed'}))
@@ -84,7 +85,7 @@ function cleanDist() {
 function watching() {
     watch(['app/scss/**/*.scss'], styles);
     watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-    watch(['app/**/*.html']).on('change', browserSync.reload)
+    watch(['app/**/*.html']).on('change', browserSync.reload);
 }
 
 
